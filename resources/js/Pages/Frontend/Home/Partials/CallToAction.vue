c0f64515831431db75021bfb77612b20a60e93e8
ilhamriSKY <ilhamrisky21@gmail.com>
2023-12-06 10:37:30 +0700

    fix

---
+++ resources/js/Pages/Frontend/Home/Partials/CallToAction.vue
@@ -0,0 +1,79 @@
+<template>
+    <section class="cta__area p-relative z-index-1">
+        <div class="container">
+            <div class="cta__wrapper-2 p-relative">
+                <div class="cta__shape">
+                    <img class="cta__shape-1" src="assets/img/user/cta-shape-1.png" alt="cta__shape" />
+                    <img class="cta__shape-2" src="assets/img/user/cta-shape-2.png" alt="cta__shape" />
+                    <img class="cta__shape-3" src="assets/img/cta/2/cta-shape-3.png" alt="cta__shape" />
+
+                    <!-- <div class="circle-wrap">
+                        <div class="circle-image">
+                            <img src="assets/img/user/ilhamtext.png" alt="circle-image">
+                        </div>
+                    </div> -->
+
+                </div>
+                <div class="row align-items-center">
+                    <div class="col-xxl-6 col-lg-6">
+
+
+                        <div class="cta__thumb-2">
+                            <img  width="350" v-lazy="'assets/img/user/ilham.jpg'"
+                                alt="cta_thumbnail" />
+                        </div>
+
+                    </div>
+                    <div class="col-xxl-6 col-lg-6">
+                        <div class="cta__content-2">
+                            <h3>
+                                {{ trans('Empowering Front-End Magic with Expert Back-End Mastery') }}
+                            </h3>
+                            <p>
+                                {{ trans(`Backend Developer with 3 years of experience. Proficient in creating web
+                                applications,
+                                chatbots, and APIs. Good knowledge in PHP, Python, and JavaScript. Experienced in
+                                using Laravel and building chatbots. Familiar with collaborative development using Git.
+                                Passionate about creating efficient and scalable solutions.`) }}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+<script setup>
+"use strict";
+const props = defineProps(['cta'])
+import Tilt from 'vanilla-tilt-vue'
+
+const options = {
+    glare: false,
+}
+</script>
+<style>
+.circle-wrap .circle-image {
+    width: 100%;
+    max-width: 150px;
+    aspect-ratio: 1/1;
+    position: absolute;
+    top: 85%;
+    left: 35%;
+    transform: translate(-50%, -50%);
+    filter: brightness(0) invert(1);
+    mix-blend-mode: exclusion;
+    z-index: 99;
+}
+
+.circle-wrap .circle-image img {
+    width: 100%;
+    animation: rotate360 15s linear infinite forwards;
+}
+
+@keyframes rotate360 {
+    100% {
+        transform: rotate(360deg);
+    }
+}
+</style>
--- resources/js/Pages/Frontend/Home/Partials/Slider-Profile.vue
+++ resources/js/Pages/Frontend/Home/Partials/Slider-Profile.vue
@@ -6,6 +6,7 @@ import { Swiper, SwiperSlide } from 'swiper/vue'
 import { onMounted, ref, computed } from 'vue'
 import axios from 'axios'
 import SliderSkeleton from './SliderSkeleton.vue'
+
 // swiper styles
 import 'swiper/css'
 import 'swiper/css/navigation'
@@ -63,11 +64,10 @@ const like = (id) => {
 }
 
 const options = {
-    glare: true,
-    "max-glare": 2,
-    scale: 1.1,
-    "glare-prerender": false,
+    glare: false,
+    scale: 1,
     gyroscope: true,
+    max: 25,
 }
 </script>
 
@@ -75,38 +75,48 @@ const options = {
     <SliderSkeleton v-if="loading" />
     <section class="slider__area pb-140 pt-120 p-relative fix">
         <div class="slider__active-2 slider__bid-area" v-show="sliders.length > 0 && !loading">
+
+            <div class="slider__shape">
+                <img class="slider__shape-1" src="assets/img/slider/2/slider-shape-1.png" alt="slider__shape" />
+                <img class="slider__shape-2" src="assets/img/slider/2/slider-shape-2.png" alt="slider__shape" />
+            </div>
+
             <div class="single-slider d-flex align-items-center">
                 <div class="container">
                     <div class="row">
                         <div class="col-xxl-6 col-xl-6 col-lg-6">
                             <div class="slider__content-2">
-                                <h4 class="slider__title-2">
-                                    {{ trans('Empowering') }}, <span> {{ trans('Front-End') }}</span>
-                                    {{
-                                        trans(`Magic with Expert`)
-                                    }}
+                                <h3 class="slider__title-sm">
+                                    <span class="wave">👋</span> {{ trans('Hello, My Name Is Ilham') }}
+                                </h3>
+
+                                <h3 class="slider__title-2">
+                                    {{ trans('I am') }}
+
                                     <span class="has-shape">
-                                        {{ trans('Back-End') }}
+                                        {{ trans('BACK-END') }}
                                         <img class="slider-title-shape" src="assets/img/icon/slider-stoke-shape.svg"
                                             alt="title" />
                                     </span>
-                                    {{ trans('Mastery') }}
-                                </h4>
-                                <p>
-                                    {{
-                                        trans(`If you are an artist and want to be a successful artist then
-                                    learn to how to create digital art with prompt.`)
-                                    }}
-                                </p>
+
+                                    {{ trans('DEVELOPER') }}
+                                </h3>
+
+                                <h3 class="slider__title-3">
+                                    {{ trans(`I've been developing websites for 3 years, specializing in web applications,
+                                    chatbots, and APIs. I'm skilled in PHP, Python, and JavaScript, with expertise in
+                                    Laravel and chatbot development. I'm also experienced in collaborative coding using Git.
+                                    My focus is on crafting efficient and scalable solutions.`) }}
+                                </h3>
 
 
-                                <div class="slider__btn-2 d-sm-flex align-items-center">
-                                    <Link :href="route('about')" class="tp-btn-border text-white header-button active mr-15 mb-15">
-                                    {{ trans('About') }}
-                                    </Link>
-                                    <Link :href="route('download.cv')" class="tp-btn-border mb-15">
-                                    {{ trans('Download CV') }}
-                                    </Link>
+                                <div class="d-sm-flex align-items-center mt-4">
+                                    <button @click="scrollToEl1" class="tp-btn-border header-button active mr-15 mb-15">
+                                        {{ trans('About') }}
+                                    </button>
+
+                                    <a :href="route('download.cv')" class="tp-btn-border active mr-15 mb-15">{{
+                                        trans("Download CV") }}</a>
                                 </div>
 
                             </div>
@@ -114,17 +124,12 @@ const options = {
                         <div class="col-xxl-6 col-xl-6 col-lg-6">
                             <div class="slider__bid-item pl-70">
                                 <div class="slider__bid-item-bg"></div>
-                                <Tilt :parallax="true" :options="options">
-                                    <Swiper @slideChange="onSwiper" style="z-index: -10" :slides-per-view="1"
-                                        :space-between="10" :modules="modules" :navigation="navigationOptions" :loop="true"
-                                        :effect="'fade'">
-                                        <SwiperSlide class="slider__bid-thumb" v-for="slider in sliders" :key="slider">
-                                            <img width="350" :src="slider.preview" alt="preview" />
-                                        </SwiperSlide>
-                                    </Swiper>
+                                <Tilt data-tilt-full-page-listening :parallax="true" :options="options">
+                                    <div class="product__details-thumb-content">
+                                        <img width="350" v-lazy="'assets/img/user/ilham.jpg'">
+
+                                    </div>
                                 </Tilt>
-
-
                             </div>
                         </div>
                     </div>
@@ -164,4 +169,38 @@ const options = {
     background-color: var(--tp-common-black);
     color: var(--tp-common-white);
 }
-</style>
+
+.slider__title-sm {
+    font-size: 18px;
+    font-weight: 200;
+    margin-bottom: 0;
+    color: var(--tp-common-white);
+}
+
+.active-light-mode .slider__title-sm {
+    font-size: 18px;
+    font-weight: 200;
+    margin-bottom: 0;
+    color: var(--tp-common-black);
+}
+
+.active-light-mode .slider__title {
+    font-size: 30px;
+    font-weight: 700;
+    color: var(--tp-common-black);
+    margin-bottom: 0;
+}
+
+.slider__title-3 {
+    font-size: 20px;
+    font-weight: 200;
+    color: var(--tp-common-white);
+    margin-bottom: 0;
+}
+
+.active-light-mode .slider__title-3 {
+    font-size: 20px;
+    font-weight: 200;
+    color: var(--tp-common-black);
+    margin-bottom: 0;
+}</style>
